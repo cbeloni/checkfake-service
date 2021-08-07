3869f44cd37e50ed026df312340cda42e3eb5ebc
cbeloni <cbeloni@gmail.com>
2021-08-07 01:26:08 -0300

    Ajustes testes de integração memeControllre

--- src/test/java/br/com/checkfakeservice/controller/MemeControllerTest.java
+++ src/test/java/br/com/checkfakeservice/controller/MemeControllerTest.java
@@ -4,24 +4,37 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
 
 import lombok.extern.slf4j.Slf4j;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
 @Slf4j
+@ActiveProfiles(profiles = "local")
 public class MemeControllerTest {
-
+	
+	@Autowired
+	private TestRestTemplate restTemplate;
 	
 	@Test
 	public void salvaMeme() throws IOException {
@@ -33,10 +46,10 @@ public class MemeControllerTest {
 
 
         HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
-        String serverUrl = "http://localhost:8082/spring-rest/fileserver/singlefileupload/";
-        RestTemplate restTemplate = new RestTemplate();
+        String serverUrl = "/meme/salvar";
         ResponseEntity<String> response = restTemplate.postForEntity(serverUrl, requestEntity, String.class);
         log.info("Response code: " + response.getStatusCode());
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 	
 	@Test
@@ -44,10 +57,10 @@ public class MemeControllerTest {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.MULTIPART_FORM_DATA);
 
-        String serverUrl = "http://localhost:8080/listar";
-        RestTemplate restTemplate = new RestTemplate();
+        String serverUrl = "/meme/listarPaginado?pageNo=1&pageSize=1&sortBy=id";
         ResponseEntity<String> response = restTemplate.getForEntity(serverUrl, String.class);
         log.info("Response code: " + response.getStatusCode());
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 	
 	public Resource getTestFile() throws IOException {
@@ -57,4 +70,30 @@ public class MemeControllerTest {
         return new FileSystemResource(testFile.toFile());
     }
 	
+	public String postFile(String filename, byte[] someByteArray, String serverUrl) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        MultiValueMap<String, String> fileMap = new LinkedMultiValueMap<>();
+        ContentDisposition contentDisposition = ContentDisposition
+                .builder("form-data")
+                .name("file")
+                .filename(filename)
+                .build();
+        fileMap.add(HttpHeaders.CONTENT_DISPOSITION, contentDisposition.toString());
+        HttpEntity<byte[]> fileEntity = new HttpEntity<>(someByteArray, fileMap);
+
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("file", fileEntity);
+
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+        
+        ResponseEntity<String> response = restTemplate.exchange(
+        		serverUrl,
+                HttpMethod.POST,
+                requestEntity,
+                String.class);
+        return response.getBody();
+
+    }
 }
